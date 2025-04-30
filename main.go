d026660f047b0e928d698572b56ce21a9d94d437
zaaripzha <student@ALEM-F4-L03-05.alem.school>
2025-04-30 13:25:25 +0500

    .

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Missing command")
-		os.Exit(1)
 	}
 
 	command := os.Args[1]
@@ -22,6 +23,46 @@ func main() {
 }
 
 func validate(args []string) {
-	var numbers []string
+	if len(args) == 0 {
+		fmt.Println("Please enter creditcard number.")
+	}
 
+	numbers := []string{}
+	if args[0] == "--stdin" {
+		d := make([]byte, 0)
+		n, error := os.Stdin.Read(d)
+		if error != nil {
+			fmt.Println("Can not read from stdin")
+		}
+
+		input := string(d[:n])
+		numbers = strings.Fields(input)
+	} else {
+		numbers = args
+	}
+}
+
+func luhnAlgorithm(number string) bool {
+	sum := 0
+	doubleDigit := false
+
+	for i := len(number); i >= 0; i-- {
+		digit, error := strconv.Atoi(string(number[i]))
+		if error != nil {
+			return false
+		}
+		if doubleDigit {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		sum += digit
+		doubleDigit = !doubleDigit
+	}
+
+	if sum%10 == 0 {
+		return true
+	}
+	return false
 }
