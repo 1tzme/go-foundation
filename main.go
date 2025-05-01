f54320bc832ba1ff2b9e59476e9456a6dbc6209b
zaaripzha <zakir.itsme@gmail.com>
2025-05-01 20:17:35 +0500

    123

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 	"strings"
 )
 
@@ -67,22 +67,24 @@ func validate(args []string) {
 
 // luhn algorithm for creaditcard num checking
 func luhnAlgorithm(number string) bool {
-    sum := 0
-    isSecond := false
+	sum := 0
+	isSecond := false
 
-    for i := len(number) - 1; i >= 0; i-- {
-        digit := int(number[i] - '0')
-        if isSecond {
-            digit *= 2
-        }
-        sum += digit / 10
-        sum += digit % 10
+	for i := len(number) - 1; i >= 0; i-- {
+		digit := int(number[i] - '0')
+		if isSecond {
+			digit *= 2
+		}
+		sum += digit / 10
+		sum += digit % 10
 
-        isSecond = !isSecond
-    }
+		isSecond = !isSecond
+	}
 	if sum%10 == 0 {
 		return true
 	}
 	return false
 }
-funcminkfniso
+
+/*
+funcminkfniso */
