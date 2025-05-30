29377a5fc78d34fb284b90a343bb3ddc2bb631ff
1tzme <zakir.itsme@gmail.com>
2025-05-30 19:41:24 +0500

    upd main output

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
     }
     defer file.Close()
 
-    var csvparser parser.CSVParser = MyCSVParser{}
+    var csvparser parser.CSVParser = &parser.MyCSVParser{}
 
     for {
         line, err := csvparser.ReadLine(file)
@@ -23,8 +23,18 @@ func main() {
             if err == io.EOF {
                 break
             }
-            fmt.Println("Error reading line:", err)
+            fmt.Println("Error reading line: ", err)
             return
+        }
+
+        fmt.Printf("Line: %s\n", line)
+        for i := 0; i < csvparser.GetNumberOfFields(); i++ {
+            field, err := csvparser.GetField(i)
+            if err != nil {
+                fmt.Printf("Error getting field %d: %v\n", i, err)
+                continue
+            }
+            fmt.Printf("Field %d: %s\n", i, field)
         }
     }
 }
