b52deb63956ddd9592e148abcfd2133b44a908bc
1tzme <zakir.itsme@gmail.com>
2025-06-02 14:08:55 +0500

    upd parser, quotes handling

--- parser/parser.go
+++ parser/parser.go
@@ -18,21 +18,21 @@ type MyCSVParser struct {
 func (p *MyCSVParser) ReadLine(r io.Reader) (string, error) {
 	p.buffer = p.buffer[:0]
 	temp := [1]byte{}
-    isCR := false
+	isCR := false
 
 	for {
 		n, err := r.Read(temp[:])
 		if n > 0 {
 			ch := temp[0]
-            if ch == '\n' && isCR {
-                isCR = true
-                break
-            }
+			if ch == '\n' && isCR {
+				isCR = true
+				break
+			}
 			if ch == '\n' {
 				break
 			}
 			if ch == '\r' {
-                isCR = true
+				isCR = true
 				continue
 			}
 			p.buffer = append(p.buffer, ch)
@@ -46,9 +46,9 @@ func (p *MyCSVParser) ReadLine(r io.Reader) (string, error) {
 		}
 	}
 
-    if len(p.buffer) == 0 {
-        return "", nil
-    }
+	if len(p.buffer) == 0 {
+		return "", nil
+	}
 
 	line := string(p.buffer)
 	fields, err := parseCSVLine(line)
@@ -76,34 +76,34 @@ func parseCSVLine(line string) ([]string, error) {
 	fields := []string{}
 	field := []byte{}
 	inQuotes := false
-    quotedField := false
+	quotedField := false
 
 	for i := 0; i < len(line); i++ {
 		switch line[i] {
 		case '"':
-            if !inQuotes {
-                inQuotes = true
-                quotedField = true
-            } else {
-                if i+1 < len(line) && line[i+1] == '"' {
-                    field = append(field, '"')
-                    i++
-                } else {
-                    inQuotes = false
-                }
-            }
+			if !inQuotes {
+				inQuotes = true
+				quotedField = true
+			} else {
+				if i+1 < len(line) && line[i+1] == '"' {
+					field = append(field, '"')
+					i++
+				} else {
+					inQuotes = false
+				}
+			}
 		case ',':
 			if inQuotes {
 				field = append(field, line[i])
 			} else {
 				fields = append(fields, string(trimSpace(field)))
 				field = field[:0]
-                quotedField = false
+				quotedField = false
 			}
 		default:
-            if !inQuotes && len(field) == 0 && line[i] == ' ' {
-                continue
-            }
+			if !inQuotes && len(field) == 0 && line[i] == ' ' {
+				continue
+			}
 			field = append(field, line[i])
 		}
 	}
@@ -111,27 +111,27 @@ func parseCSVLine(line string) ([]string, error) {
 	if inQuotes {
 		return nil, ErrQuote
 	}
-    if quotedField {
-        if len(field) > 0 && field[0] == '"' {
-            field = field[1:]
-        }
-        if len(field) > 0 && field[len(field)-1] == '"' {
-            field = field[:len(field)-1]
-        }
-    }
+	if quotedField {
+		if len(field) > 0 && field[0] == '"' {
+			field = field[1:]
+		}
+		if len(field) > 0 && field[len(field)-1] == '"' {
+			field = field[:len(field)-1]
+		}
+	}
 
 	fields = append(fields, string(trimSpace(field)))
 	return fields, nil
 }
 
 func trimSpace(s []byte) []byte {
-    start := 0
-    end := len(s)
-    for start < end && s[start] == ' ' {
-        start++
-    }
-    for end > start && s[start] == ' ' {
-        end--
-    }
-    return s[start:end]
+	start := 0
+	end := len(s)
+	for start < end && s[start] == ' ' {
+		start++
+	}
+	for end > start && s[start] == ' ' {
+		end--
+	}
+	return s[start:end]
 }
