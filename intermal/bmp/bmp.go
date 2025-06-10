0e1a3e1aa711e4dcaa8c96e245191fe2ca112de7
armakhat <student@ALEM-F4-R01-05A.alem.school>
2025-06-10 21:23:42 +0500

    added ReadHeaders function

--- intermal/bmp/bmp.go
+++ intermal/bmp/bmp.go
@@ -1,1 +1,31 @@
 package bmp
+
+import (
+	"encoding/binary"
+	"fmt"
+	"os"
+)
+
+func ReadHeaders(path string) (*BitmapFileHeader, *DIBHeader, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	var fileHeader BitmapFileHeader
+	if err := binary.Read(file, binary.LittleEndian, &fileHeader); err != nil {
+		return nil, nil, fmt.Errorf("failed to read file header: %w", err)
+	}
+
+	if fileHeader.FileType != [2]byte{'B', 'M'} {
+		return nil, nil, fmt.Errorf("not a BMP file: invalid signature %v", fileHeader.FileType)
+	}
+
+	var dibHeader DIBHeader
+	if err := binary.Read(file, binary.LittleEndian, &dibHeader); err != nil {
+		return nil, nil, fmt.Errorf("failed to read DIB header: %w", err)
+	}
+
+	return &fileHeader, &dibHeader, nil
+}
--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"bitmap/intermal/bmp"
+	"fmt"
+	"log"
+)
+
+func main() {
+	fileHeader, dibHeader, err := bmp.ReadHeaders("sample.bmp")
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	fmt.Println("BMP Header:")
+	fmt.Printf("- FileType %s\n", fileHeader.FileType)
+	fmt.Printf("- FileSizeInBytes %d\n", fileHeader.FileSize)
+	fmt.Printf("- HeaderSize %d\n", fileHeader.PixelDataOffset)
+
+	fmt.Println("DIB Header:")
+	fmt.Printf("- DibHeaderSize %d\n", dibHeader.HeaderSize)
+	fmt.Printf("- WidthInPixels %d\n", dibHeader.Width)
+	fmt.Printf("- HeightInPixels %d\n", dibHeader.Height)
+	fmt.Printf("- PixelSizeInBits %d\n", dibHeader.BitsPerPixel)
+	fmt.Printf("- ImageSizeInBytes %d\n", dibHeader.ImageSize)
+}
