5e6ca78607ab5198b800d29d1cb1b2df5ad183e0
1tzme <zakir.itsme@gmail.com>
2025-07-18 16:01:47 +0500

    feat: add flag init, usage

---
+++ internal/flags/flags.go
@@ -0,0 +1,29 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"own-redis/internal/utils"
+)
+
+const DefaultPort = 8080
+
+func FlagInit() int {
+	help := flag.Bool("help", false, "Show help message")
+	port := flag.Int("port", DefaultPort, "Port number")
+
+	flag.Usage = utils.Usage
+	flag.Parse()
+
+	if *help {
+		utils.Usage()
+		os.Exit(0)
+	}
+
+	if *port < 1 || *port > 65535 {
+		fmt.Fprintf(os.Stderr, "Error: Port number %d is out of valid range\n", *port)
+	}
+
+	return *port
+}
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"own-redis/internal/flags"
+)
+
 func main() {
-	
+	port := flags.FlagInit()
 }
