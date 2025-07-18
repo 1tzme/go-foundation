b23bfeec35c680ac4c835a949ac3a9d72be39e5b
1tzme <zakir.itsme@gmail.com>
2025-07-18 16:35:24 +0500

    feat: add server struct, start, request handler

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"own-redis/internal/flags"
+	"own-redis/internal/server"
 )
 
 func main() {
 	port := flags.FlagInit()
+
+	server := server.NewServer(port)
+	err := server.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Server error: %v\n", err)
+		os.Exit(1)
+	}
 }
